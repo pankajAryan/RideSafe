a59588d185003e27817abdf82cb4f8130191d57a
Pankaj Yadav <>
2018-03-09 02:32:50 +0530

    Login screen ui done with stackviews

--- RideSafe/Controller/LoginController.swift
+++ RideSafe/Controller/LoginController.swift
@@ -13,10 +13,40 @@ import MaterialActivityIndicator
 class LoginController: UIViewController {
     
     var model:Register?
+    @IBOutlet weak var officialCheckBox: Checkbox!
+    @IBOutlet weak var citizenCheckBox: Checkbox!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        citizenCheckBox.uncheckedBorderColor = .black
+        citizenCheckBox.borderStyle = .circle
+        citizenCheckBox.checkedBorderColor = .blue
+        citizenCheckBox.checkmarkColor = .blue
+        citizenCheckBox.checkmarkStyle = .circle
+        
+        citizenCheckBox.valueChanged = { (value) in
+            print("checkbox value change: \(value)")
+            if value == true {
+                self.officialCheckBox.isChecked = false
+            }
+        }
+        
+        officialCheckBox.uncheckedBorderColor = .black
+        officialCheckBox.borderStyle = .circle
+        officialCheckBox.checkedBorderColor = .blue
+        officialCheckBox.checkmarkColor = .blue
+        officialCheckBox.checkmarkStyle = .circle
+
+        officialCheckBox.valueChanged = { (value) in
+            print("checkbox value change: \(value)")
+            if value == true {
+                self.citizenCheckBox.isChecked = false
+            }
+        }
+    
     }
+
     
     @IBAction func btnClicked(_ sender: Any) {
         
