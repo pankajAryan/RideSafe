3f062ed633164814b8af37ca93e3548ace166c5f
Anand Mishra <anandmishra@Anands-MacBook-Air.local>
2018-03-19 07:50:47 +0530

    Added Classes of road infra

---
+++ RideSafe/RoadInfraIssue/RoadInfraIssueViewController.swift
@@ -0,0 +1,24 @@
+//
+//  RoadInfraIssueViewController.swift
+//  RideSafe
+//
+//  Created by Anand Mishra on 19/03/18.
+//  Copyright © 2018 Mobiquel. All rights reserved.
+//
+
+import UIKit
+
+class RoadInfraIssueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
