65585fd11dac98abcaf46b18b87469cde43e3a12
Anand Mishra <anandmishra@Anands-MacBook-Air.local>
2018-03-23 22:58:28 +0530

    My Report controller Added

---
+++ RideSafe/Reports/ReportTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  ReportTableViewCell.swift
+//  RideSafe
+//
+//  Created by Anand Mishra on 23/03/18.
+//  Copyright © 2018 Mobiquel. All rights reserved.
+//
+
+import UIKit
+
+class ReportTableViewCell: UITableViewCell {
+    @IBOutlet weak var issueImageView: UIImageView!
+    @IBOutlet weak var vehicleNumberLable: UILabel!
+    @IBOutlet weak var vehicleDiscriptionLabel: UILabel!
+    @IBOutlet weak var tagsLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var pendingImageView: UIImageView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func navigationButtonClicked(_ sender: Any) {
+    }
+    
+    @IBAction func actionClicked(_ sender: Any) {
+    }
+    
+}
---
+++ RideSafe/Reports/ReportsContainerViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ReportsContainerViewController.swift
+//  RideSafe
+//
+//  Created by Anand Mishra on 23/03/18.
+//  Copyright © 2018 Mobiquel. All rights reserved.
+//
+
+import UIKit
+import SWSegmentedControl
+
+class ReportsContainerViewController: UIViewController {
+
+    @IBOutlet weak var reportSegmentController: SWSegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func reportSegementClicked(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
