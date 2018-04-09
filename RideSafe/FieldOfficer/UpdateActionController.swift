82f075484839ba3c456d41ccc8011079d118fd2d
Pankaj Yadav <pankajaryan05@gmail.com>
2018-04-09 20:35:02 +0530

    UI changes

--- RideSafe/FieldOfficer/UpdateActionController.swift
+++ RideSafe/FieldOfficer/UpdateActionController.swift
@@ -12,6 +12,7 @@ import UIDropDown
 class UpdateActionController: RideSafeViewController {
 
     @IBOutlet weak var dropDownView: UIView!
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var updateButton: UIButton!
     @IBOutlet weak var actionTextView: UITextView!
     @IBOutlet weak var nameLabel: UILabel!
@@ -24,6 +25,7 @@ class UpdateActionController: RideSafeViewController {
     var phoneNumber = ""
     var issueStatus = ""
     
+    @IBOutlet weak var callingButton: UIButton!
     @IBOutlet weak var pendingRadioButton: Checkbox!
     @IBOutlet weak var resolveRadioButton: Checkbox!
     var drivingIssue: DrivingIssueForFieldOfficial?
@@ -33,13 +35,21 @@ class UpdateActionController: RideSafeViewController {
         self.title = "Update Action, Case # \(drivingIssue?.drivingIssueId ?? "")"
         self.imageUploaded?.image = uploadedImage
         self.descriptionLabel.text = drivingIssue?.description
-        self.dateLabel.text = drivingIssue?.createdOn
+        self.dateLabel.text = (drivingIssue?.createdOn)! + " |"
         self.nameLabel.text = drivingIssue?.postedByName
         self.phoneNumber = drivingIssue?.postedByMobile ?? ""
         self.actionTextView.placeholder = "Action Taken"
         self.actionTextView.text = drivingIssue?.action
         self.updateButton.isHidden = drivingIssue?.status?.uppercased() == "PENDING" ? false : true
+        callingButton.isHidden = self.updateButton.isHidden
         self.actionTextView.isEditable = drivingIssue?.status?.uppercased() == "PENDING" ?  true : false
+        if (drivingIssue?.status?.uppercased() == "PENDING") {
+            dropDownView.isHidden = false
+        } else {
+            dropDownView.isHidden = true
+
+        }
+        statusLabel.text = drivingIssue?.status
         
         if self.updateButton.isHidden {
             if drivingIssue?.status?.uppercased() == "RESOLVED" {
@@ -56,6 +66,19 @@ class UpdateActionController: RideSafeViewController {
         }
         setBackButton()
         setupUI()
+    }
+    
+    @IBAction func callClicked(_ sender: UIButton) {
+        if let url = URL(string: "tel://\(phoneNumber)"), UIApplication.shared.canOpenURL(url) {
+            if #available(iOS 10, *) {
+                UIApplication.shared.open(url)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+    @IBAction func commentClicked(_ sender: UIButton) {
     }
     
     private func setupUI() {
