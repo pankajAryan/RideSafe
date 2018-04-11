2908d80dacbff2707abf081669ac94a614010752
Anand Mishra <anandmishra@Anands-MacBook-Air.local>
2018-04-11 12:15:20 +0530

    Notification class added

---
+++ RideSafe/Notification/NotificationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NotificationTableViewCell.swift
+//  RideSafe
+//
+//  Created by Anand Mishra on 11/04/18.
+//  Copyright © 2018 Mobiquel. All rights reserved.
+//
+
+import UIKit
+
+class NotificationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
